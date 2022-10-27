3033e91b76467466df03a0968dd730f2557dc584
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2022-10-28 01:53:17 +0530

    Update MainNavigation.js

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -7,7 +7,7 @@ import FavoritesContext from "../../store/favorites-context";
 function MainNavigation() {
   const favoritesCtx = useContext(FavoritesContext);
 
-  return (
+  return ( 
     <header className={classes.header}>
       <div className={classes.logo}>React Meetups</div>
       <nav>
