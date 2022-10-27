38e238c6133f1cc50d5095b552800d47780b222c
Ar <106869558+Hungrycoddie@users.noreply.github.com>
2022-10-27 16:43:20 +0530

    Add data to firebase

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,20 +1,34 @@
-import { useState } from "react"; 
+import { useState, useEffect } from "react"; 
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
   const [isloading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
-
-
-  fetch
-    ("https://react01-getting-started-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json")
+  useEffect(() => {
+    setIsLoading(true);
+    fetch
+      ("https://react01-getting-started-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json")
     
-    .then(response => {
-      return response.json();
-    }).then(data => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    }); 
+      .then(response => {
+        return response.json();
+      }).then(data => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+   
 
   if (isloading) {
     return (
